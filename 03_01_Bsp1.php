5d6cbab829c5d9017cfe1e71700d614e03908a99
markus-fichtinger <markus-fichtinger@gmx.at>
2016-11-28 17:39:29 +0100

    First file

---
+++ 03_01_Bsp1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MEDT - Beispieil 1</title>
+</head>
+<body>
+
+	<h1>Beispiel 1</h1>
+
+	<form method="post">
+
+	Ihre Eingabe:
+	<input type="text" name="txt">
+
+	<br><br>
+
+	<input type="submit" name="exploreBtn" value="Explode">
+	<input type="submit" name="resetBtn" value="Reset">
+
+	<form method="post">
+
+
+
+	<?php
+
+	if(isset($_POST['exploreBtn'])) {
+
+	$text = $_POST['txt'];
+	$arr = explode(" ", $text);
+
+		echo "<br>"; echo "<br>"; echo "<br>";
+
+		echo "Ihre Eingabe als Liste";
+
+		echo "<ul>";
+		foreach ($arr as $item) {
+			echo "<li>$item</li>";
+		}
+		echo "</ul>";
+	}
+
+	?>
+
+</body>
+</html>
