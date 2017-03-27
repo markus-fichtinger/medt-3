aa99ff3cd60354594f7c12b557ea0fc87a93e0d5
markus-fichtinger <m.fichtinger1@gmx.at>
2017-03-27 22:14:27 +0200

    Test_Bsp_03

---
+++ hue/Test_Bsp_03.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formularberarbeitungx</title>
+	<meta charstet="UTF-8">
+</head>
+<body>
+
+	<p>Die gewünschten Urlaubstage bitte auswählen:</p>
+
+	<?php
+	echo "<form action=\"".$_SERVER['PHP_SELF']."\" method=post>";
+	?>
+
+	<input type="checkbox" name="vacation[]" value="mo">Montag<br>
+	<input type="checkbox" name="vacation[]" value="di">Dienstag<br>
+	<input type="checkbox" name="vacation[]" value="mi">Mittwoch<br>
+	<input type="checkbox" name="vacation[]" value="do">Donnerstag<br>
+	<input type="checkbox" name="vacation[]" value="fr">Freitag<br><br>
+
+	<input type="submit" name="requestVacationtn" value="Abschicken">
+
+	<?php
+	echo "</form>";
+
+	if(isset($_POST['requestVacationtn']))
+	{
+		$holiday = $_POST['vacation'];
+		if(in_array('fr', $holiday))
+			echo "<br><p>SIE KÖNENN SICH AM FREITAG KEINEN URLAUB NEHMEN!</p>";
+
+		else
+		{
+			echo "<h3>Ihre Urlaubstage: </h3>";
+			?><ul><?php
+			foreach ($holiday as $item)
+			{
+				echo "<li>".$item."</li>";
+			}
+			?></ul><?php
+		}
+		
+	}
+
+
+	?>
+
+</body>
+</html>
